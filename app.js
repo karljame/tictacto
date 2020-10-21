ff45d8cd89ef2f8eb959648c30bb45516b6a16d6
karljame <karl321jame@gmail.com>
2020-10-21 14:11:26 +0300

    ye

--- app.js
+++ app.js
@@ -10,27 +10,65 @@ const cellDivs = document.querySelectorAll('.cell')
 cellDivs.forEach(el => {
     console.log(el)
     el.addEventListener('click', event => {
-        
+
         const selectedcell= event.currentTarget
         const selectedrow = selectedcell.dataset.row
         const selectedcol = selectedcell.dataset.col
-        
-             if ( gameboard[selectedrow][selectedcol] == '-') { 
+
+             if ( gameboard[selectedrow][selectedcol] == '-') {
                 selectedcell.innerText = 'x'
 
                 gameboard[selectedrow][selectedcol] = 'x'
 
-                if(gameboard[0][0] != ' ')
-                {
-                    if(gameboard[0][0] == gameboard[0][1] && gameboard[0][1] == gameboard[0][2])
-            
-                {
-                    winner = gameboard[0][0];
+                if (gameboard[0][0] == 'x' && gameboard[0][1] == 'x' && gameboard[0][2] == 'x') {
                     console.log('winner')
-            }
                 }
+                
+                if (gameboard[1][0] == 'x' && gameboard[1][1] == 'x' && gameboard[1][2] == 'x') {
+                    console.log('winner')
+                }
+                
+                if (gameboard[2][0] == 'x' && gameboard[2][1] == 'x' && gameboard[2][2] == 'x') {
+                    console.log('winner')
+                  
+                }
+                
+                if (gameboard[0][0] == 'x' && gameboard[1][0] == 'x' && gameboard[2][0] == 'x') {
+                    console.log('winner')
+                }
+                
+                if (gameboard[0][1] == 'x' && gameboard[1][1] == 'x' && gameboard[2][1] == 'x') {
+                    console.log('winner')
+                }
+                
+                if (gameboard[0][2] == 'x' && gameboard[1][2] == 'x' && gameboard[2][2] == 'x') {
+                    console.log('winner')
+                }
+                
+                if (gameboard[0][0] == 'x' && gameboard[1][1] == 'x' && gameboard[2][2] == 'x') {
+                    console.log('winner')
+                }
+                
+                if (gameboard[0][2] == 'x' && gameboard[1][1] == 'x' && gameboard[2][0] == 'x') {
+                    console.log('winner')
+                }
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
 
             }
     })
 });
-
